28b49281da69d41e7718e60d7cb71f6fbb226acf
yand <yand@outlook.com>
2022-12-07 13:33:42 +0300

    YA

--- pla09_host_monitor.py
+++ pla09_host_monitor.py
@@ -1,45 +1,32 @@
 import os
-import datetime
+from datetime import datetime as dt
 import time
 
 def host_is_healthy(host):
-
     if os.name.find("nt") >= 0:
         cmd = f"ping {host}"
 
     if os.name.find("posix") >= 0:
         cmd = f"ping {host} -c 1"
-
     result = os.popen(cmd).read().encode("windows-1251").decode("866")
 
     return result.upper().find("TTL") > 0
 
-def check_hosts(hosts):
-
-
-
-with open("data/hosts.txt") as f:
-    hosts = f.read().split("\n")
-
-
-while True:
-
+def check_hosts(hosts, log_file):
     for host in hosts:
-        if os.name.find("nt") >= 0:
-            cmd = f"ping {host}"
-
-        if os.name.find("posix") >= 0:
-            cmd = f"ping {host} -c 1"
-
-        result = os.popen(cmd).read().encode("windows-1251").decode("866")
-
-        if result.upper().find("TTL") > 0:
-            # print(f"{host} is good")
+        if host_is_healthy(host):
             pass
         else:
-            now = datetime.datetime.now()
-            print(f"{now}\t{host} is BAD")
-            with open("data/log.txt", "a", encoding="utf-8") as f:
-                f.write(f"{now}\t{host} is BAD\n")
+            print(f"{dt.now()}\t{host} is BAD")
+            with open(log_file, "a", encoding="utf-8") as f:
+                f.write(f"{dt.now()}\t{host} is BAD\n")
 
-    time.sleep(10)
+
+if __name__ == "__main__":
+
+    with open("data/hosts.txt") as f:
+        hosts = f.read().split("\n")
+
+    while True:
+        check_hosts(hosts, "data/log1.txt")
+        time.sleep(10)
