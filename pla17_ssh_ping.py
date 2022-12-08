785447d329cce90d8ff9ca31d4bdb89d39886898
yand <yand@outlook.com>
2022-12-08 15:31:00 +0300

    YA

---
+++ pla17_ssh_ping.py
@@ -0,0 +1,23 @@
+from paramiko import SSHClient
+import paramiko
+
+hosts = ["127.0.0.1", "ya.ru",  "bad.bad", "172.24.239.255"]
+
+with SSHClient() as client:
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    client.connect(
+        hostname="172.24.236.10",
+        port=22,
+        username="yand",
+        password="Pa$$w0rd"
+    )
+
+    for host in hosts:
+        print("------------------------------------------------------")
+        cmd = f"ping {host} -c 1"
+        result = client.exec_command(cmd)[1].read().decode("UTF-8")
+        # print(result)
+        if result.upper().find("TTL") > 0:
+            print(f"{host} is good")
+        else:
+            print(f"{host} is BAD")
