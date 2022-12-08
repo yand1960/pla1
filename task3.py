79630227451ee679fcb443ab27fa2d743d568767
yand <yand@outlook.com>
2022-12-08 10:26:25 +0300

    YA

--- task3.py
+++ task3.py
@@ -1,2 +1,34 @@
 # Написать программный код, который покажет пользователю в экселе
 # все товары, цена которых в диапазоне от 100 до 1000
+
+import pymssql
+import os
+from _datetime import datetime as dt
+
+with pymssql.connect(
+    server="yand.dyndns.org",
+    database="AdventureWorks",
+    user="northwind",
+    password="northwind"
+) as conn:
+
+    sql = "SELECT ProductID, Name, ProductNumber, ListPrice FROM Production.Product"
+
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
+    products = cursor.fetchall()
+
+file_name = f"products_{dt.now()}.csv"
+file_name = file_name.replace(":", "_")
+file_name = file_name.replace(" ", "_")
+
+print(file_name)
+
+with open(f"data/{file_name}", "w") as f:
+    f.write("ID;Название;Артикул;Цена\n")
+    for p in products:
+        print(f"{p[1]:40}\t\t{p[3]}")
+        f.write(f"{p[0]};{p[1]};{p[2]};{p[3]}\n")
+
+os.system(f"start Excel data\\{file_name}")
