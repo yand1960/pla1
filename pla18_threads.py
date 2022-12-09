ea111b44a2807e608572a4d2afb41bb587cf3144
yand <yand@outlook.com>
2022-12-09 13:25:53 +0300

    YA

--- pla18_threads.py
+++ pla18_threads.py
@@ -9,7 +9,7 @@ def measure(n):
     result = 10 * n # симуляция измерения
     lock.acquire() # один из механизмов синхронизации
     # print(n, result)
-    results.append(result)
+    results.append([n, result])
     lock.release()
     return result
 
@@ -20,8 +20,21 @@ if __name__ == "__main__":
     #     result = measure(n)
     #     print(n, result)
 
+    pool = []
     for n in range(1, 10):
         thread = Thread(target=measure, args=(n,))
+        pool.append(thread)
         thread.start()
 
-    print(results)
+    # time.sleep(3.5) # дешево и сердито
+
+    # while len(results) < 9:
+    #     time.sleep(0.1)
+
+    for thread in pool:
+        thread.join() # ждем, пока поток закончит работу
+
+    results.sort()
+    # print(results)
+    for measurement in results:
+        print(f"Датчик {measurement[0]}: {measurement[1]}")
